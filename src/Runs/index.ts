0a1d8a7d3ef4cb94a6f58f2b7705e7957af732a3
mattjacobson <matthew.g.jacobson@gmail.com>
2025-01-27 13:43:21 -0500

    update 0.0.11 - adding new patternFill run

--- src/Runs/index.ts
+++ src/Runs/index.ts
@@ -2,10 +2,12 @@ import { AutoFill } from './AutoFill';
 import { ClassicSatin } from './ClassicSatin';
 import { Run } from './Run';
 import { Satin } from './Satin';
+import { PatternFill } from './PatternFill';
 
 export const Runs = {
   Run,
   Satin,
   ClassicSatin,
   AutoFill,
+  PatternFill,
 };
